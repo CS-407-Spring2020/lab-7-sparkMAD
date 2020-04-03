a165aad054f08800d93907065a08bad108fcd0bc
sanghyunpark <sanwisco99@gmail.com>
2020-04-02 23:50:28 -0500

    test 1

--- app/src/main/java/com/example/lab7/MainActivity.java
+++ app/src/main/java/com/example/lab7/MainActivity.java
@@ -1,13 +1,57 @@
 package com.example.lab7;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationManagerCompat;
+
 import android.os.Bundle;
+import android.widget.EditText;
+import android.view.View;
+import android.app.Notification;
+import androidx.core.app.NotificationCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    private NotificationManagerCompat notificationManager;
+    private EditText editTextTitle;
+    private EditText editTextMessage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        notificationManager = NotificationManagerCompat.from(this);
+
+        editTextTitle = findViewById(R.id.editText);
+        editTextMessage = findViewById(R.id.editText2);
+    }
+
+    public void sendOnChannel1(View v) {
+        String title = editTextTitle.getText().toString();
+        String message = editTextMessage.getText().toString();
+
+        Notification notification = new NotificationCompat.Builder(this, com.example.lab7.App.CHANNEL_1_ID)
+                .setSmallIcon(R.drawable.ic_chat_black_24dp)
+                .setContentTitle(title)
+                .setContentText(message)
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setCategory(NotificationCompat.CATEGORY_MESSAGE)
+                .build();
+
+        notificationManager.notify(1, notification);
+    }
+
+    public void sendOnChannel2(View v) {
+        String title = editTextTitle.getText().toString();
+        String message = editTextMessage.getText().toString();
+
+        Notification notification = new NotificationCompat.Builder(this, com.example.lab7.App.CHANNEL_2_ID)
+                .setSmallIcon(R.drawable.ic_send_black_24dp)
+                .setContentTitle(title)
+                .setContentText(message)
+                .setPriority(NotificationCompat.PRIORITY_LOW)
+                .build();
+
+        notificationManager.notify(2, notification);
     }
 }
